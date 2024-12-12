598e5f9c18fe838fb0c0dcaddaebcdd871475d2b
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-12-12 00:40:13 -0300

    chore: created the header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,52 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <main className="prose prose-invert prose-headings:text-zinc-100 prose-headings:font-semibold prose-h1:text-3xl prose-h2:text-xl prose-h2:font-medium prose-p:text-zinc-400 prose-pre:bg-zinc-900 prose-pre:border prose-pre:border-white/10 prose-pre:rounded-2xl prose-a:text-cyan-500">
+      <h1 id="authentication">Authentication</h1>
+      <p>
+        You&#39;ll need to authenticate your requests to access any of the
+        endpoints in the Protocol API. In this guide, we&#39;ll look at how
+        authentication works. Protocol offers two ways to authenticate your API
+        requests: Basic authentication and OAuth2 with a token.
+      </p>
+      <h2 id="basic-authentication">Basic authentication</h2>
+      <p>
+        With basic authentication, you use your username and password to
+        authenticate your HTTP requests. Unless you have a very good reason, you
+        probably shouldn&#39;t use basic auth. Here&#39;s how to authenticate
+        using cURL:
+      </p>
+      <pre>
+        <code className="language-bash">
+          curl https://api.protocol.chat/v1/conversations \ -u username:password
+        </code>
+      </pre>
+      <p>Please don&#39;t commit your Protocol password to GitHub!</p>
+      <h2 id="oauth2-with-bearer-token">OAuth2 with bearer token</h2>
+      <p>
+        The recommended way to authenticate with the Protocol API is by using
+        OAuth2. When establishing a connection using OAuth2, you will need your
+        access token — you will find it in the{' '}
+        <a href="#">Protocol dashboard</a> under API settings. Here&#39;s how to
+        add the token to the request header using cURL:
+      </p>
+      <pre>
+        <code className="language-bash">
+          curl https://api.protocol.chat/v1/conversations \ -H
+          &quot;Authorization: Bearer token&quot;
+        </code>
+      </pre>
+      <p>
+        Always keep your token safe and reset it if you suspect it has been
+        compromised.
+      </p>
+      <h2 id="using-an-sdk">Using an SDK</h2>
+      <p>
+        If you use one of our official SDKs, you won&#39;t have to worry about
+        any of the above — fetch your access token from the{' '}
+        <a href="#">Protocol dashboard</a> under API settings, and the client
+        library will take care of the rest. All the client libraries use OAuth2
+        behind the scenes.
+      </p>
+    </main>
+  )
 }
